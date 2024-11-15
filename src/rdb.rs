c1ccccaa40c0c37bdbd9164f530ec771a06abc6e
Nazar <nezar.ras@gmail.com>
2024-11-15 16:34:31 +0200

    codecrafters submit [skip ci]

--- src/rdb.rs
+++ src/rdb.rs
@@ -37,6 +37,21 @@ pub fn load_rdb_file(
     Ok(())
 }
 
+fn handle_keys_values(buffer: &[u8], pos: &mut usize, keys_values: &mut Vec<(String, String)>) {
+    while *pos < buffer.len() && buffer[*pos] != 0xFF {
+        // The 1-byte flag that specifies the value’s type and encoding
+        *pos += 1;
+        let key_length = buffer[*pos] as usize;
+        let (key, new_pos) = read_string(&buffer, *pos + 1, key_length);
+        let value_length = buffer[new_pos] as usize;
+        let (value, new_pos) = read_string(&buffer, new_pos + 1, value_length);
+        keys_values.push((key, value));
+        *pos = new_pos; // Update the position for the next iteration
+        print!("{}", pos);
+        println!("{:?}", keys_values);
+    }
+}
+
 fn parse_rdb_key_value(buffer: &[u8]) -> Option<Vec<(String, String)>> {
     let mut keys_values = Vec::new();
     let mut pos = 9; // Start after the header "REDIS0011"
@@ -44,22 +59,15 @@ fn parse_rdb_key_value(buffer: &[u8]) -> Option<Vec<(String, String)>> {
     while pos < buffer.len() {
         match buffer[pos] {
             0xFE => { // Database section marker
-                println!("Found database section marker at position {}", pos);
                 pos += 2;
-                println!("wherei am at: {}:{}", pos, buffer[pos]);
                 // Skip over hash table resize information (0xFB)
                 if buffer[pos] == 0xFB {
-                    pos += 4; // Skip the 4 bytes of hash table size info
-                    println!("Skipped resize hash table info at position {}", pos);
+                    pos += 3; // Skip the 4 bytes of hash table size info
                 }
-                println!("where i am at: {}:{}", pos, buffer[pos]);
-                let length: usize = buffer[pos] as usize;
-                let (key, new_pos) = read_string(&buffer, pos+1, length);
-                let length: usize =  buffer[new_pos] as usize;
-                let (value, new_pos) = read_string(&buffer, new_pos + 1, length);
-                keys_values.push((key, value));
+                handle_keys_values(buffer, &mut pos, &mut keys_values);
+                break;
             }
-            , _ => {
+            _ => {
                 pos += 1; // Skip any other unexpected sections
             }
         }
@@ -73,6 +81,7 @@ fn parse_rdb_key_value(buffer: &[u8]) -> Option<Vec<(String, String)>> {
     }
 }
 
+
 fn read_string(buffer: &[u8], pos: usize, length: usize) -> (String, usize) {
     // Convert the slice to a String. Adjust error handling as needed.
     let string = String::from_utf8(buffer[pos..pos + length].to_vec())
@@ -80,29 +89,6 @@ fn read_string(buffer: &[u8], pos: usize, length: usize) -> (String, usize) {
 
     // Return the String and the new position (pos + length)
     (string, pos + length)
-}
-
-// Decode length based on Redis size-encoding
-fn decode_length(buffer: &[u8], pos: usize) -> Option<(usize, usize)> {
-    let byte = buffer[pos];
-    let length = match byte >> 6 {
-        0b00 => (byte & 0x3F) as usize, // 6-bit length
-        0b01 => {
-            let next_byte = buffer.get(pos + 1)?;
-            (((byte & 0x3F) as usize) << 8 | (*next_byte as usize)) as usize // 14-bit length
-        },
-        0b10 => {
-            let length_bytes = &buffer[pos + 1..pos + 5];
-            u32::from_be_bytes(length_bytes.try_into().ok()?) as usize // 32-bit length
-        },
-        _ => {
-            println!("Unexpected length encoding at position {}", pos);
-            return None;
-        }
-    };
-
-    println!("Decoded length: {}, at position: {}", length, pos);
-    Some((length, pos + 1)) // Return length and new position
 }
 
 
