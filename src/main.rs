bbd7a2d7062990d44160c9f48458644e81067484
Nazar <nezar.ras@gmail.com>
2024-11-17 09:36:58 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -25,33 +25,20 @@ async fn main() -> tokio::io::Result<()> {
             .expect("Failed to load RDB file");
     }
 
-    let config_for_replica = Arc::clone(&config);
     {
-        let config_lock = config_for_replica.lock().unwrap();
-    
+        let config_lock = config.lock().unwrap();
+
         // If the server is configured as a replica, initiate a connection to the master
         if let Some(replica_of) = config_lock.replica.clone() {
             let parts: Vec<&str> = replica_of.split_whitespace().collect();
             if parts.len() == 2 {
                 let master_host = parts[0].to_string(); // Clone as `String` for 'static lifetime
                 let master_port = parts[1].to_string(); // Clone as `String` for 'static lifetime
-    
+                let replica_port = config_lock.port.clone(); // Use the replica's own port
+
                 tokio::spawn(async move {
-                    match tokio::net::TcpStream::connect(format!("{}:{}", master_host, master_port)).await {
-                        Ok(mut master_stream) => {
-                            println!("Connected to master at {}:{}", master_host, master_port);
-    
-                            // Send the PING command as part of the handshake
-                            let ping_command = "*1\r\n$4\r\nPING\r\n";
-                            if master_stream.write_all(ping_command.as_bytes()).await.is_err() {
-                                eprintln!("Failed to send PING command to master");
-                                return;
-                            }
-                            println!("Sent PING command to master");
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to connect to master: {:?}", e);
-                        }
+                    if let Err(e) = perform_replica_handshake(master_host, master_port, replica_port).await {
+                        eprintln!("Handshake failed: {:?}", e);
                     }
                 });
             } else {
@@ -59,7 +46,6 @@ async fn main() -> tokio::io::Result<()> {
             }
         }
     }
-    
 
     let listener;
     {
@@ -105,5 +91,65 @@ async fn main() -> tokio::io::Result<()> {
                 }
             }
         });
+    }
+}
+
+
+/// Handles the replica-to-master handshake process
+async fn perform_replica_handshake(master_host: String, master_port: String, replica_port: String) -> tokio::io::Result<()> {
+    match tokio::net::TcpStream::connect(format!("{}:{}", master_host, master_port)).await {
+        Ok(mut master_stream) => {
+            println!("Connected to master at {}:{}", master_host, master_port);
+
+            // 1. Send PING
+            let ping_command = "*1\r\n$4\r\nPING\r\n";
+            master_stream.write_all(ping_command.as_bytes()).await?;
+            println!("Sent PING command to master");
+
+            // Wait for +PONG
+            let mut buffer = vec![0; 512];
+            let n = master_stream.read(&mut buffer).await?;
+            if &buffer[..n] != b"+PONG\r\n" {
+                eprintln!("Unexpected response to PING: {:?}", &buffer[..n]);
+                return Err(tokio::io::Error::new(tokio::io::ErrorKind::Other, "Unexpected PING response"));
+            }
+            println!("Received PONG from master");
+
+            // 2. Send REPLCONF listening-port <PORT>
+            let replconf_listening_port = format!(
+                "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${}\r\n{}\r\n",
+                replica_port.len(),
+                replica_port
+            );
+            master_stream.write_all(replconf_listening_port.as_bytes()).await?;
+            println!("Sent REPLCONF listening-port command");
+
+            // Wait for +OK
+            let n = master_stream.read(&mut buffer).await?;
+            if &buffer[..n] != b"+OK\r\n" {
+                eprintln!("Unexpected response to REPLCONF listening-port: {:?}", &buffer[..n]);
+                return Err(tokio::io::Error::new(tokio::io::ErrorKind::Other, "Unexpected REPLCONF listening-port response"));
+            }
+            println!("Received OK for REPLCONF listening-port");
+
+            // 3. Send REPLCONF capa psync2
+            let replconf_capa_psync2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+            master_stream.write_all(replconf_capa_psync2.as_bytes()).await?;
+            println!("Sent REPLCONF capa psync2 command");
+
+            // Wait for +OK
+            let n = master_stream.read(&mut buffer).await?;
+            if &buffer[..n] != b"+OK\r\n" {
+                eprintln!("Unexpected response to REPLCONF capa psync2: {:?}", &buffer[..n]);
+                return Err(tokio::io::Error::new(tokio::io::ErrorKind::Other, "Unexpected REPLCONF capa psync2 response"));
+            }
+            println!("Received OK for REPLCONF capa psync2");
+
+            Ok(())
+        }
+        Err(e) => {
+            eprintln!("Failed to connect to master: {:?}", e);
+            Err(e)
+        }
     }
 }
